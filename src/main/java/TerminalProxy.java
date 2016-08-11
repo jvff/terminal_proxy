fe7b9e0666a8a9be8b183ce5dadce56352639b6c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-08-11 13:33:57 +0000

    Create initial TerminalProxy class
    - Spawns an interactive bash process and forwards data between it and an
      external connection

---
+++ src/main/java/TerminalProxy.java
@@ -0,0 +1,58 @@
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TerminalProxy {
+    public static void main(String[] args) throws Exception {
+        try {
+            safeMain();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void safeMain() throws Exception {
+        Process bash = new ProcessBuilder("/bin/bash", "-i").start();
+        InputStream bashInputStream = bash.getInputStream();
+        OutputStream bashOutputStream = bash.getOutputStream();
+
+        PrintWriter writer = new PrintWriter(bashOutputStream);
+        InputStreamReader reader = new InputStreamReader(bashInputStream);
+
+        ServerSocket listener = new ServerSocket(15100);
+        Socket socket = listener.accept();
+
+        forwardKeys(socket.getInputStream(), bashOutputStream);
+        forwardKeys(bashInputStream, socket.getOutputStream());
+
+        bash.waitFor();
+    }
+
+    private static void forwardKeys(InputStream in, OutputStream out) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    safelyForwardKeys(in, out);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private static void safelyForwardKeys(InputStream in, OutputStream out)
+            throws Exception {
+        int data = in.read();
+
+        while (data >= 0) {
+            out.write(data);
+            out.flush();
+            data = in.read();
+        }
+    }
+}
