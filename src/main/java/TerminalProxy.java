9c727902a6e09c6ae1d0b0c97d361eb917935f98
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-09-21 12:16:51 +0000

    Collect and forward environment variables

--- src/main/java/TerminalProxy.java
+++ src/main/java/TerminalProxy.java
@@ -1,10 +1,12 @@
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import jpty.JPty;
 import jpty.Pty;
@@ -20,7 +22,7 @@ public class TerminalProxy {
 
     private static void safeMain() throws Exception {
         String[] command = new String[] { "/bin/bash", "-i" };
-        String[] environment = new String[] { "TERM=xterm" };
+        String[] environment = buildEnvironmentVariables();
 
         Pty bash = JPty.execInPTY(command[0], command, environment);
         InputStream bashInputStream = bash.getInputStream();
@@ -36,6 +38,38 @@ public class TerminalProxy {
         forwardKeys(bashInputStream, socket.getOutputStream());
 
         bash.waitFor();
+    }
+
+    private static String[] buildEnvironmentVariables() {
+        Map<String, String> environmentVariables = getEnvironmentVariables();
+        int numberOfEnvironmentVariables = environmentVariables.size();
+        ArrayList<String> collectedVariables =
+                new ArrayList<String>(numberOfEnvironmentVariables);
+        String[] resultingArray = new String[numberOfEnvironmentVariables];
+
+        for (String variable : environmentVariables.keySet()) {
+            String value = environmentVariables.get(variable);
+
+            collectedVariables.add(variable + "=" + value);
+        }
+
+        return collectedVariables.toArray(resultingArray);
+    }
+
+    private static Map<String, String> getEnvironmentVariables() {
+        Map<String, String> allVariables = new LinkedHashMap<>();
+        Map<String, String> currentVariables = System.getenv();
+
+        allVariables.putAll(currentVariables);
+
+        addCustomEnvironmentVariables(allVariables);
+
+        return allVariables;
+    }
+
+    private static void addCustomEnvironmentVariables(
+            Map<String, String> environmentVariables) {
+        environmentVariables.put("TERM", "xterm");
     }
 
     private static void forwardKeys(InputStream in, OutputStream out) {
